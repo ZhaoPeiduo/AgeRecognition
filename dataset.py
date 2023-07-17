4ff5426748a100c2f230fedf348310d23e34d2d2
Zhao Peiduo <zhaopeiduo@gmail.com>
2023-07-17 10:44:20 +0800

    add id mapping

--- dataset.py
+++ dataset.py
@@ -2,7 +2,7 @@ from torch.utils.data import Dataset
 import pandas as pd
 
 class AgeRecognitionDataset(Dataset):
-    def __init__(self, triplet_csv_path, image_dir, preprocesor, cust_transforms=None):
+    def __init__(self, triplet_csv_path, image_dir, preprocessor, cust_transforms=None):
         self.record = pd.read_csv(triplet_csv_path)
         self.image_dir = image_dir
         self.preprocessor = preprocessor
@@ -13,7 +13,7 @@ class AgeRecognitionDataset(Dataset):
 
     def __getitem__(self, idx):
         entry = self.record.iloc[idx]
-        anchor = preprocessor.preprocess(entry.anchor, cust_transforms=self.cust_transforms)
-        positive = preprocessor.preprocess(entry.positive, cust_transforms=self.cust_transforms)
-        negative = preprocessor.preprocess(entry.negative, cust_transforms=self.cust_transforms)
+        anchor = self.preprocessor.preprocess(entry.anchor, cust_transforms=self.cust_transforms)
+        positive = self.preprocessor.preprocess(entry.positive, cust_transforms=self.cust_transforms)
+        negative = self.preprocessor.preprocess(entry.negative, cust_transforms=self.cust_transforms)
         return (anchor, positive, negative)
